c5fc0e0e8d47596246b2f9539d9b93d3e3b4bb69
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-06-02 08:17:55 -0600

    Update to reassign variables and print new values

--- variables.rb
+++ variables.rb
@@ -1,6 +1,8 @@
 name = "Noelle"
+puts name
 birthdate = "12/27/1997"
 home_town = "Fort Collins, CO"
+puts home_town
 age = 25
 pets = 2
 mugs = 10
@@ -10,8 +12,10 @@ rent = 2700.00
 awake = true
 asleep = false
 ready_to_learn = true
-name # "Noelle E"
-home_town # "Paris, France"
+name = "Noelle E"
+puts name
+home_town = "Paris, France"
+puts home_town
 puts "Time to learn"
 puts "Happy Mod0 Week"
 puts "Caffeine and pickles can help migraines"
